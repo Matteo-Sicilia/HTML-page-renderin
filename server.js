c173cd67fac5438b9c85995149c096c8f7a48b18
Matteo-Sicilia <Matteo.Sicilia@outlook.it>
2024-04-19 17:19:21 +0200

    Refactor CSS styles and add new dependencies in package.json and server.js

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import fastifyView from "@fastify/view";
 import ejs from "ejs";
+import fastifyPostgres from "@fastify/postgres";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-const fruits = ["Banana", "Pera", "Mela", "Kiwi", "Fragola"];
 
 export default async function createServer() {
     const app = fastify({
@@ -32,8 +31,28 @@ export default async function createServer() {
         layout: "template.ejs",
     });
 
+    await app.register(fastifyPostgres, {
+        host: "127.0.0.1",
+        port: 8080,
+        database: "lezioni_node",
+        user: "postgres",
+        password: "pinocembro",
+    });
+
     app.get("/", async (req, res) => {
-        return res.view("./views/index.ejs", { fruits: fruits });
+        const del = req.query.delete;
+
+        if (del) {
+            const delResult = await app.pg.query(
+                "DELETE FROM todos WHERE id = $1",
+                [del]
+            );
+            if (delResult.rowCount === 0) {
+                throw new Error("Not found");
+            }
+        }
+        const result = await app.pg.query("SELECT * FROM todos");
+        return res.view("./views/index.ejs", { todos: result.rows });
     });
 
     app.get("/about", async (req, res) => {
